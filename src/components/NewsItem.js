f51b8bcede58528d48aa67527e95e93f1bf857d2
ShakirAhmad <shakirmca16@gmail.com>
2023-08-18 10:37:21 +0530

    minor updates

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
 
 			<div className='my-3'>
 				<div className="card" style={{ width: "18rem" }}>
-					<img src={imageUrl} className="card-img-top" alt="..." />
+					<img src={!imageUrl ? 'https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I' : imageUrl} className="card-img-top" alt="..." />
 					<div className="card-body">
 						<h5 className="card-title">{title}...</h5>
 						<p className="card-text">{description}...</p>
-						<a href={newsUrl} className="btn btn-sm btn-primary" target='__blank'>Read More</a>
+						<a href={newsUrl} className="btn btn-sm btn-dark" target='__blank'>Read More</a>
 					</div>
 				</div>
 			</div>

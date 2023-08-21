08d16a42fb0fd8b1c21bf09073e47d5f7dbb949d
ShakirAhmad <shakirmca16@gmail.com>
2023-08-21 23:06:45 +0530

    changed class based components to function based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-	render() {
-		let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-		return (
-			<div className='container'>
-				<div className=' my-3'>
-					<div className="card" >
-						<div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
-							<span className="posibadge rounded-pill bg-danger">
-								{source}
-							</span>
-						</div>
-						<img src={!imageUrl ? 'https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I' : imageUrl} className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">{title}...</h5>
-							<p className="card-text">{description}...</p>
-							<p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
-							<a href={newsUrl} className="btn btn-sm btn-dark" target='__blank'>Read More</a>
-						</div>
+	let { title, description, imageUrl, newsUrl, author, date, source } = props;
+	return (
+		<div className='container'>
+			<div className=' my-3'>
+				<div className="card" >
+					<div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+						<span className="posibadge rounded-pill bg-danger">
+							{source}
+						</span>
+					</div>
+					<img src={!imageUrl ? 'https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I' : imageUrl} className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">{title}...</h5>
+						<p className="card-text">{description}...</p>
+						<p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+						<a href={newsUrl} className="btn btn-sm btn-dark" target='__blank'>Read More</a>
 					</div>
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
+
 }
 
 export default NewsItem
